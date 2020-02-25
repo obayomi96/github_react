63953e38ba738f6c21a1e8469d1675fd66f7c63d
martins obayomi <obayomimartins96@gmail.com>
2020-02-25 01:19:24 +0100

    Implements search github profile Search github user profile by username List github profile repositories Click to view readme contents

--- src/actions/githubActions.js
+++ src/actions/githubActions.js
@@ -1,16 +1,21 @@
-import { GET_USER_PROFILE, GET_USER_REPOS } from "./types";
 import axios from "axios";
 
-export const fetchUserProfile = () => async dispatch => {
+import {
+  GET_USER_PROFILE,
+  GET_USER_REPOS,
+  GET_REPO_README,
+} from "./types";
+
+export const fetchUserProfile = (username) => async (dispatch) => {
   try {
     const response = await axios.get(
-      `https://api.github.com/users/obayomi96?client_id=57cd7f12ba532256fd43&client_secret=149e945a07ea5c17e72f62129cbb07284055242c`
+      `https://api.github.com/users/${username}?client_id=57cd7f12ba532256fd43&client_secret=149e945a07ea5c17e72f62129cbb07284055242c`
     );
     console.log("res", response);
     if (response.status = 200) {
       return dispatch({
         type: GET_USER_PROFILE,
-        payload: response
+        payload: response.data
       });
     }
   } catch (err) {
@@ -18,16 +23,34 @@ export const fetchUserProfile = () => async dispatch => {
   }
 };
 
-export const fetchUserRepos = () => async dispatch => {
+export const fetchUserRepos = (username) => async (dispatch) => {
   try {
     const response = await axios.get(
-      `https://api.github.com/users/obayomi96/repos?client_id=57cd7f12ba532256fd43&client_secret=149e945a07ea5c17e72f62129cbb07284055242c`
+      `https://api.github.com/users/${username}/repos?client_id=57cd7f12ba532256fd43&client_secret=149e945a07ea5c17e72f62129cbb07284055242c`
     );
     console.log("repo res", response);
     if (response.status === 200) {
       return dispatch({
         type: GET_USER_REPOS,
-        payload: response
+        payload: response.data
+      });
+    }
+  } catch (err) {
+    console.log("repo err", err);
+  }
+};
+
+export const fetchRepoReadme = (owner, repo) => async (dispatch) => {
+  try {
+    const path = 'README.md';
+    const response = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/contents/${path}?client_id=57cd7f12ba532256fd43&client_secret=149e945a07ea5c17e72f62129cbb07284055242c`
+    );
+    console.log("repo readme", response);
+    if (response.status === 200) {
+      return dispatch({
+        type: GET_REPO_README,
+        payload: response.data
       });
     }
   } catch (err) {
